0608feb6ae3408b3d2d76a414099676ba47b0725
Bruno Antunes <bmvantunes@gmail.com>
2020-02-23 19:24:39 +0000

    after recording

---
+++ src/pages/List.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+export default function List({ownersList}) {
+  // const [owners, setOwners] = useState([]);
+  // useEffect(() => {
+  //   async function loadData() {
+  //     const response = await fetch('http://localhost:4001/vehicles');
+  //     const ownersList = await response.json();
+  //     setOwners(ownersList);
+  //   }
+
+  //   loadData();
+  // }, []);
+  
+  return (
+    <div>
+      {ownersList.map((e, index) => (
+        <div key={index}>
+          <Link as={`/${e.vehicle}/${e.ownerName}`} href="/[vehicle]/[person]">
+            <a>
+              Navigate to {e.ownerName}'s {e.vehicle}
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+List.getInitialProps = async () => {
+  const response = await fetch('http://localhost:4001/vehicles');
+  const ownersList = await response.json();
+  return {ownersList: ownersList}
+}
--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,7 +1,44 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-export default function Person() {
-    const router = useRouter();
-    console.log(router.query);
-    return <h2>{router.query.person}'s {router.query.vehicle}</h2>
+export default function Person({ ownersList }) {
+  const router = useRouter();
+
+ const [owners, setOwners] = useState(ownersList);
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch('http://localhost:4001/vehicles?ownerName=' +
+      router.query.person +
+      '&vehicle=' +
+      router.query.vehicle);
+      const ownersList = await response.json();
+      setOwners(ownersList);
+    }
+
+    if(ownersList.length == 0) {
+        loadData();
+    }
+  }, []);
+
+  if(!owners[0]) { 
+      return <div>loading...</div>
+  }
+
+  return <pre>{owners[0]?.details}</pre>;
 }
+
+Person.getInitialProps = async ctx => {
+    if(!ctx.req) {
+        return { ownersList: [] };
+    }
+
+  const { query } = ctx;
+  const response = await fetch(
+    'http://localhost:4001/vehicles?ownerName=' +
+      query.person +
+      '&vehicle=' +
+      query.vehicle
+  );
+  const ownersList = await response.json();
+  return { ownersList: ownersList };
+};
